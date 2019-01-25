400008fac86749a97c8ba567295877911e18a0a2
Adam Chester <xpnsec@protonmail.com>
2019-01-25 00:12:33 +0000

    Added mangle.ps1

---
+++ Mangle.ps1
@@ -0,0 +1,13 @@
+﻿function GetRandomString {
+    return -join ((97..122) | Get-Random -Count 16 | % {[char]$_})
+}
+
+$seatbeltRand = GetRandomString
+
+Get-ChildItem $Env:BUILD_SOURCESDIRECTORY -Filter "*.cs" -recurse |
+    Foreach-Object {
+        $c = ($_ | Get-Content)
+        $c = $c -replace 'Seatbelt',"$seatbeltRand"
+        $c = $c -replace 'ShowLogo();',""
+        [IO.File]::WriteAllText($_.FullName, ($c -join "`r`n"))
+    }
